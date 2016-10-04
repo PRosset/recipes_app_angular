705bdb751048623519470fa0d5655e3e4d7429d9
Chandler Kelley <chandler.t.kelley@gmail.com>
2016-10-04 11:57:44 -0400

    Can retrieve data on a single recipe from Yummly API

--- routes/api.js
+++ routes/api.js
@@ -42,12 +42,36 @@ router.get('/', function(req, res, next) {
 	}).end();
 });
 
-// Recipe from external API
-router.get('/yummly/:keyword', function (req, res, next) {
-	console.log("Request for Yummly hit received!")
+// List of recipes from external API
+router.get('/yummly-keyword/:keyword', function (req, res, next) {
 	var options = {
 		host: "api.yummly.com",
 		path: "/v1/api/recipes?_app_id=678354c2&_app_key=5068cc5b8f8cc221124e6726a3622062&q=" + req.params.keyword,
+		method: "get"
+	};
+	http.request(options, function(response) {
+		
+	  	var body = '';
+
+		//another chunk of data has been recieved, so append it to `str`
+		response.on('data', function (chunk) {
+			body += chunk;
+			});
+
+		//the whole response has been recieved, so we just print it out here
+		response.on('end', function () {
+			var parsed = JSON.parse(body)
+			res.json(parsed);
+		});
+	}).end();
+});
+
+// Individual recipe from external API
+router.get('/yummly-id/:id', function (req, res, next) {
+	console.log("Request for Yummly hit received!")
+	var options = {
+		host: "api.yummly.com",
+		path: "/v1/api/recipe/" + req.params.id + "?_app_id=678354c2&_app_key=5068cc5b8f8cc221124e6726a3622062",
 		method: "get"
 	};
 	http.request(options, function(response) {
