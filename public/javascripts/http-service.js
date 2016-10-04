ae7b8603f7d88ee6cfaa176e262f51683c46cbeb
Chandler Kelley <chandler.t.kelley@gmail.com>
2016-10-04 11:37:45 -0400

    Can make calls to Yummly Api, display results of the call in a form page, and scroll through the different recipes that came back

--- public/javascripts/http-service.js
+++ public/javascripts/http-service.js
@@ -1,10 +1,10 @@
 angular.module("recipesApp")
 .service("httpService", function($http) {
 	this.indexRecipes = function () {
-		return $http.get("/api")
+		return $http.get("/api");
 	}
-    this.recipeFromApi = function () {
-        // return $http.get( HTTP call to Node server )
+    this.recipeFromApi = function (keyword) {
+        return $http.get("/api/yummly/" + keyword);
     }
     this.addRecipe = function (recipe) {
         // return $http.post(" address for our own Node server ", recipe)
--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,74 @@
 var express = require('express');
 var router = express.Router();
 var http = require('http');
+// var getCallback = function(response) {
+//   	var body = '';
+
+// 	//another chunk of data has been recieved, so append it to `str`
+// 	response.on('data', function (chunk) {
+// 		body += chunk;
+// 		});
+
+// 	//the whole response has been recieved, so we just print it out here
+// 	response.on('end', function () {
+// 		var parsed = JSON.parse(body)
+// 		res.json(parsed);
+// 		console.log(parsed);
+// 	});
+// }
 
 // Index of recipes
 router.get('/', function(req, res, next) {
-  var options = {
-  	host: "localhost",
-  	port: 3000,
-  	path: "/recipes",
-  	method: "get"
-  };
-  http.request(options, function(response) {
-  	var body = '';
+	var options = {
+		host: "localhost",
+		port: 3000,
+		path: "/recipes",
+		method: "get"
+	};
+	http.request(options, function(response) {
+		var body = '';
 
-	//another chunk of data has been recieved, so append it to `str`
-	response.on('data', function (chunk) {
-		body += chunk;
+		//another chunk of data has been recieved, so append it to `str`
+		response.on('data', function (chunk) {
+			body += chunk;
+			});
+
+		//the whole response has been recieved, so we just print it out here
+		response.on('end', function () {
+			var parsed = JSON.parse(body)
+			res.json(parsed);
+			console.log(parsed);
 		});
-
-	//the whole response has been recieved, so we just print it out here
-	response.on('end', function () {
-		var parsed = JSON.parse(body)
-		res.json(parsed);
-		console.log(parsed);
-	});
-  }).end()
+	}).end();
 });
+
+// Recipe from external API
+router.get('/yummly/:keyword', function (req, res, next) {
+	console.log("Request for Yummly hit received!")
+	var options = {
+		host: "api.yummly.com",
+		path: "/v1/api/recipes?_app_id=678354c2&_app_key=5068cc5b8f8cc221124e6726a3622062&q=" + req.params.keyword,
+		method: "get"
+	};
+	http.request(options, function(response) {
+		console.log("Request sent to Yummly!")
+		
+	  	var body = '';
+
+		//another chunk of data has been recieved, so append it to `str`
+		response.on('data', function (chunk) {
+			body += chunk;
+			});
+
+		//the whole response has been recieved, so we just print it out here
+		response.on('end', function () {
+			var parsed = JSON.parse(body)
+			res.json(parsed);
+			console.log(parsed);
+		});
+	}).end();
+});
+
+http://api.yummly.com/v1/api/recipe/Avocado-cream-pasta-sauce-recipe-306039
 
 module.exports = router;
